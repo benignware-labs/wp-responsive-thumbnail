6f00c8ccafac7cbd3f9fb2a71ca0f7fc65657117
Rafael Nowrotek <mailbenignware.com>
2018-06-06 02:56:01 +0200

    Handle image size array and other stuff

--- wp_responsive_thumbnail.php
+++ wp_responsive_thumbnail.php
@@ -4,44 +4,55 @@
  Plugin Name: WP Responsive Thumbnail
  Plugin URI: http://github.com/benignware-labs/wp-responsive-thumbnail
  Description: Responsive thumbnails per breakpoints
- Version: 0.0.1
+ Version: 0.0.2
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
 */
 
-
 function wp_responsive_thumbnail_html($html, $post_id, $post_thumbnail_id, $size, $attr) {
+  if (is_array($size)) {
+    return $html;
+  }
+  $result = wp_responsive_attachment_image($post_thumbnail_id, $size, false, $attr);
+  if ($result) {
+    return $result;
+  }
+  return $html;
+}
+
+function wp_responsive_attachment_image($attachment_id, $size, $icon = false, $attr = array()) {
   global $__responsive_image_sizes;
   if (isset($__responsive_image_sizes) && isset($__responsive_image_sizes[$size])) {
+    $html = "";
     $responsive_image_sizes = $__responsive_image_sizes[$size];
     // Check to see if a 'retina' class exists in the array when calling "the_post_thumbnail()", if so output different <img/> html
-      
-    $id = $post_thumbnail_id; // gets the id of the current post_thumbnail (in the loop)
+
+    $id = $attachment_id; // gets the id of the current post_thumbnail (in the loop)
     $src = wp_get_attachment_image_src($id, $size); // gets the image url specific to the passed in size (aka. custom image size)
-    
+
     if ($src) {
       $alt = get_the_title($id); // gets the post thumbnail title
-    
+
       $pattern = "~<img[^>]*src=['\"]" . preg_quote($src[0], "~") . "['\"][^>]*\/?>~";
       $matched = preg_match($pattern, $html, $match, PREG_OFFSET_CAPTURE);
-      
+
       $before_image = "<picture>";
       foreach ($responsive_image_sizes as $width => $image_size) {
         $img_src = wp_get_attachment_image_src($id, $image_size);
-        $before_image.= "<source media=\"(max-width: " . $width . "px)\" srcset=\"" . $img_src[0] . " " . $img_src[1] . "w" . "\"/>"; 
+        $before_image.= "<source media=\"(max-width: " . $width . "px)\" srcset=\"" . $img_src[0] . " " . $img_src[1] . "w" . "\"/>";
       }
       $after_image = "</picture>";
-      
+
       if ($matched) {
         $html = substr_replace($html, $before_image . $match[0][0] . $after_image, $match[0][1], strlen($match[0][0]));
       } else {
-        // Fallback to default img tag: 
+        // Fallback to default img tag:
         $class = isset($attr['class']) ? $attr['class'] : ''; // gets classes passed to the post thumbnail, defined here for easier function access
         $html.= $before_image . '<img src="' . $src[0] . '" class="' . $class . '" />' . $after_image;
       }
     }
-    
+
   }
   return $html;
 }
